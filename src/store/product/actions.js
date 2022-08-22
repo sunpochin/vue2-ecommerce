344d6d58f7db868f8e32fb67553edb8025c8dd9a
pac <sunpochin@gmail.com>
2022-08-22 16:12:13 +0800

    update actions

--- src/store/product/actions.js
+++ src/store/product/actions.js
@@ -28,3 +28,22 @@ export function productDetails({ commit }, id) {
 			console.log(error);
 		});
 }
+
+export function addCart({ commit, getters }, payload) {
+	let cart = getters.cart;
+	cart.push(payload);
+	commit('setCart', cart);
+}
+
+export function removeCart({ commit, getters }, id) {
+	let cart = [];
+	if (id) {
+		for (let index = 0; index < getters.cart.length; index++) {
+			const element = getters.cart[index];
+			if (element.id !== id) {
+				cart.push(element);
+			}
+		}
+	}
+	commit('setCart', cart);
+}
