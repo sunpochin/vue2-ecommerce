6942b27c854a88b81335344dd9948f09582037c0
pac <sunpochin@gmail.com>
2022-09-23 12:10:09 +0800

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	preset: '@vue/cli-plugin-unit-jest',
-	collectCoverage: false,
+	collectCoverage: true,
 	collectCoverageFrom: [
 		'src/**/*.{js,vue}',
 		'!src/main.js', // No need to cover bootstrap file
