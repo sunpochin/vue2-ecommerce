92f5387958f5b7ae7581082d9c54ca83e92727c7
pac <sunpochin@gmail.com>
2023-09-05 16:50:15 +0800

    remove vimeo

--- src/main.js
+++ src/main.js
@@ -1,39 +1,35 @@
-import Vue from 'vue';
-import App from './AppBACK.vue';
-import router from './router/index';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
 // import store from './vuex/index';
 
-import vueVimeoPlayer from 'vue-vimeo-player';
-Vue.use(vueVimeoPlayer);
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fortawesome/fontawesome-free/js/all.js';
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 // Vue.prototype.$hostname = 'http://localhost:8000';
 // Vue.prototype.$hostname = 'https://fastapi-pac.onrender.com';
 // Vue.prototype.$hostname = 'http://localhost:8081/v1';
-Vue.prototype.$hostname = 'https://pac-backend.onrender.com/v1';
+Vue.prototype.$hostname = "https://pac-backend.onrender.com/v1";
 
 Vue.use(router);
 // Vue.use(store);
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
-
 new Vue({
-	beforeCreate: function () {
-		console.log('globa: ', this.$hostname);
-	},
-	render: (h) => h(App),
-	router,
-	// store,
-}).$mount('#app');
+  beforeCreate: function () {
+    console.log("globa: ", this.$hostname);
+  },
+  render: (h) => h(App),
+  router,
+  // store,
+}).$mount("#app");
