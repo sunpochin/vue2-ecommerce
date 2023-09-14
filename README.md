66a9fb6f98d9062606e52f90be776019e6ffdd7b
pac <sunpochin@gmail.com>
2023-09-14 16:37:59 +0800

    Remove useless parts. Update readme.md

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # vue-ecommerce
 
+這是一個 Vue 電商網站 demo
+前端使用 Vue2, Vuex, Vue-Router 技術。
+後端使用 node.js .
+
 ## Live demo
+
 https://vue2-ecommerce.netlify.app/
 
 [![CircleCI](https://circleci.com/gh/sunpochin/vue2-ecommerce.svg?style=shield)](https://app.circleci.com/pipelines/github/sunpochin/vue2-ecommerce)
 [![Coverage Status](https://coveralls.io/repos/github/sunpochin/vue2-ecommerce/badge.svg?branch=main)](https://coveralls.io/github/sunpochin/vue2-ecommerce?branch=main)
 
 ## Project setup
+
 ```
 yarn install
 ```
+
 ### Unit test and Code coverage
+
 ```
 yarn test:unit
 yarn coveralls
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
-### Compiles and minifies for production
+<!-- ### Compiles and minifies for production
 ```
 yarn build
 ```
@@ -32,4 +41,4 @@ yarn lint
 ```
 
 ### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+See [Configuration Reference](https://cli.vuejs.org/config/). -->
--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,50 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import ProductList from '../components/products/ProductList.vue';
-import ProductDetail from '../components/products/ProductDetail.vue';
-import CartView from '../components/carts/CartView.vue';
-import LalaPartVue from '../components/LalaPart.vue';
-import bulbPartVue from '../components/bulbPart.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ProductList from "../components/products/ProductList.vue";
+import ProductDetail from "../components/products/ProductDetail.vue";
+import CartView from "../components/carts/CartView.vue";
 
 Vue.use(VueRouter);
 const routes = [
-	{
-		path: '/bulbs',
-		component: bulbPartVue,
-		props: true,
-	},
-	{
-		path: '/balls',
-		component: LalaPartVue,
-		props: true,
-	},
-	{
-		path: '/cate/:cateID',
-		component: ProductList,
-		name: 'category',
-		props: true,
-	},
-	{
-		path: '/products/:productId',
-		component: ProductDetail,
-		props: true,
-	},
-	{
-		path: '/cart',
-		name: 'cart',
-		component: CartView,
-	},
-	{
-		path: '/login',
-		component: CartView,
-	},
-	{
-		path: '/',
-		component: ProductList,
-	},
-	{
-		// path: "*",
-		path: '/:catchAll(.*)',
-		name: 'NotFound',
-		redirect: '/',
-		meta: {
-			requiresAuth: false,
-		},
-	},
+  {
+    path: "/cate/:cateID",
+    component: ProductList,
+    name: "category",
+    props: true,
+  },
+  {
+    path: "/products/:productId",
+    component: ProductDetail,
+    props: true,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: CartView,
+  },
+  {
+    path: "/login",
+    component: CartView,
+  },
+  {
+    path: "/",
+    component: ProductList,
+  },
+  {
+    // path: "*",
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    redirect: "/",
+    meta: {
+      requiresAuth: false,
+    },
+  },
 ];
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
-
 
 export default router;
